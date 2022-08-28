e98d252a02056ecba636cc76db1683bc16ef5b5f
IvanKovalev <deformgame@gmail.com>
2022-08-28 13:56:47 +0300

    Added Task3

---
+++ Task3/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+Console.Clear();
+Console.WriteLine("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+Console.Clear();
+Console.WriteLine($"Вы ввели число: {num}. ");
+
+int count = 1;
+while (num > 0 && count <= num)
+{
+    Console.WriteLine($"Куб числа {count} - {Math.Pow(count, 3)}");
+    count++;
+}
+while (num < 0 && count >= num)
+{
+    Console.WriteLine($"Куб числа {count} - {Math.Pow(count, 3)}");
+    count--;
+}

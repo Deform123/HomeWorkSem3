cc702f40c4327f5bd766909e8fcc3837d21282dc
IvanKovalev <deformgame@gmail.com>
2022-08-28 13:01:21 +0300

    Added Task1

---
+++ Task1/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+Console.Clear();
+Console.WriteLine("Введите пятизначное число: ");
+int userNumber = Convert.ToInt32(Console.ReadLine());
+string number = Convert.ToString(userNumber);
+
+if (number.Length < 5 )
+    Console.WriteLine("Введенное Вами число имеет меньше пяти знаков. "); 
+else if ( number[0] == number[4] && number[1] == number[3] ) 
+    Console.WriteLine($"Введенное вами число {userNumber} является палиндромом.");
+else if ( number[0] != number[4] || number[1] != number[3] )
+    Console.WriteLine($"Введенное вами число {userNumber} не является палиндромом.");
